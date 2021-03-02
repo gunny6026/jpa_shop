bdee52fef58ea01e90765982428db447fc033e01
gunny6026 <gunny6026@naver.com>
2021-03-02 15:57:05 +0900

    JPA 라이브러리 설정 & h2 DB 테스트케이스 완료

---
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
